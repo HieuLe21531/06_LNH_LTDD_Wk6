cf20641d8520456e75f9181fc6a7eff8ef364b4f
HieuLe21531 <kaibaseto2001@gmail.com>
2024-10-07 12:48:46 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import Screen1 from '../Lab6/assets/web/Screen1';
+import Screen2 from '../Lab6/assets/web/Screen2';
+export default function App() {
+  return (
+      <>
+        <Screen2/>
+      </>
+
+  );
+}
